c82ff89445a6b6a8eda78c7e3c6c1d5d06d7945b
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-06-07 17:52:57 +0600

    added bd collection create

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abef6se.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abef6se.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -12,7 +14,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
 
 app.use(cors());
 app.use(express.json());
@@ -23,12 +24,13 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
+    const tasksCollection = db.collection("tasks");
+    const annotationsCollection = db.collection("annotations");
 
-
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
     //  await client.close();
